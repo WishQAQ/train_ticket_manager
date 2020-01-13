b9fc008b0acd4526b7f50ee6fa237001294618d9
wish <wishtime1024@gmail.com>
2020-01-13 10:36:24 +0800

    修改单张购票点击范围

--- src/components/TrainTimesTable/index.vue
+++ src/components/TrainTimesTable/index.vue
@@ -301,9 +301,9 @@
           <el-dropdown trigger="click">
             <el-button size="mini">操作</el-button>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item><el-button size="mini" type="text" @click="jumpPayTicket(scope.row,scope.$index)" v-if="scope.row.ticket_status === 0 || scope.row.ticket_status === 3 || scope.row.ticket_status === 4">购票</el-button></el-dropdown-item>
-              <el-dropdown-item><el-button size="mini" type="text" @click="jumpEditTicket(scope.row,'refund')" v-if="scope.row.ticket_status === 1 || scope.row.ticket_status === 3">退票</el-button></el-dropdown-item>
-              <el-dropdown-item><el-button size="mini" type="text" @click="jumpEditTicket(scope.row,'change')" v-if="scope.row.ticket_status === 1 || scope.row.ticket_status === 4">改签</el-button></el-dropdown-item>
+              <el-dropdown-item><el-button class="jumpTicketWeb" size="mini" type="text" @click="jumpPayTicket(scope.row,scope.$index)" v-if="scope.row.ticket_status === 0 || scope.row.ticket_status === 3 || scope.row.ticket_status === 4">购票</el-button></el-dropdown-item>
+              <el-dropdown-item><el-button class="jumpTicketWeb" size="mini" type="text" @click="jumpEditTicket(scope.row,'refund')" v-if="scope.row.ticket_status === 1 || scope.row.ticket_status === 3">退票</el-button></el-dropdown-item>
+              <el-dropdown-item><el-button class="jumpTicketWeb" size="mini" type="text" @click="jumpEditTicket(scope.row,'change')" v-if="scope.row.ticket_status === 1 || scope.row.ticket_status === 4">改签</el-button></el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
 
@@ -572,5 +572,10 @@
         }
       }
     }
+  }
+  /deep/.jumpTicketWeb{
+    width: 100%;
+    height: 100%;
+    text-align: left;
   }
 </style>
